c0a60e95f9d21c8ee98fa63d44d53387097137da
DESKTOP-FLRUCDN\ADMIN <phw96015@naver.com>
2020-11-19 11:08:11 +0900

    기본 자바스크립트 수업 자료

---
+++ lid.js
@@ -0,0 +1,30 @@
+function over2(obj) {
+    obj.src = "./images/duli.png";
+}
+
+function out2(obj) {
+    obj.src = "./images/duli2.jpg";
+}
+
+// var img4 = document.getElementById("img4")
+// img4.addEventListener("mouseover", function () {
+//     img4.src = "./images/duli.png"
+// });
+// img4.addEventListener("mouseout", function () {
+//     img4.src = "./images/duli3.jpg"
+// });
+
+
+var img4 = document.getElementById("img4")
+img4.addEventListener("mouseover", over3 );
+img4.addEventListener("mouseout", over3 );
+
+function over3(obj) {
+    obj.src = "./images/simpson.gif";
+}
+
+function out3(obj) {
+    obj.src = "./images/simpson2.gif";
+
+}
+
